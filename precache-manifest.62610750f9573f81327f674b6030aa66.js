2bd8277ebd20dc53347732ee2e712d647b862414
antonioducs <antonioduarte.cs@gmail.com>
2019-09-07 13:41:13 -0300

    Updates

---
+++ precache-manifest.62610750f9573f81327f674b6030aa66.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91168b416bdc37497ab82e3a60a21031",
+    "url": "/cad/index.html"
+  },
+  {
+    "revision": "b4e1bc492166eabef975",
+    "url": "/cad/static/css/main.d9d4f888.chunk.css"
+  },
+  {
+    "revision": "bc912476243347e665d1",
+    "url": "/cad/static/js/2.785f8017.chunk.js"
+  },
+  {
+    "revision": "b4e1bc492166eabef975",
+    "url": "/cad/static/js/main.e2631261.chunk.js"
+  },
+  {
+    "revision": "7c22143a9acc8bab5cce",
+    "url": "/cad/static/js/runtime~main.4741121f.js"
+  },
+  {
+    "revision": "aa6e25d9bd5a0e81a7481b4bf98958f7",
+    "url": "/cad/static/media/CAD LOGO 2.aa6e25d9.png"
+  },
+  {
+    "revision": "c7a4548f69aa83778b84585f5bb558b9",
+    "url": "/cad/static/media/Raleway-BoldItalic.c7a4548f.ttf"
+  },
+  {
+    "revision": "69a7be3c42464fbfd497dfb564dcd8ae",
+    "url": "/cad/static/media/Team2.69a7be3c.png"
+  },
+  {
+    "revision": "c7fd104685c3dd7ed7a3c0c13dac6b7c",
+    "url": "/cad/static/media/inscEncerradas.c7fd1046.jpg"
+  },
+  {
+    "revision": "ef29daed02503bf52dd511055a760022",
+    "url": "/cad/static/media/logo.ef29daed.gif"
+  },
+  {
+    "revision": "3861567faac27c02b11fb78424d19b67",
+    "url": "/cad/static/media/slide1.3861567f.png"
+  },
+  {
+    "revision": "a2f8d4e25ca99491e4606e64b080ad34",
+    "url": "/cad/static/media/slide2.a2f8d4e2.png"
+  },
+  {
+    "revision": "9a513477bfbe439b218106cb9abedbd5",
+    "url": "/cad/static/media/slide3.9a513477.png"
+  },
+  {
+    "revision": "4ee61e6aa228d044072ef3d4c5e846e5",
+    "url": "/cad/static/media/speaker1.4ee61e6a.png"
+  },
+  {
+    "revision": "7aa5624ec7ccb41c1400fe47ae31e38f",
+    "url": "/cad/static/media/speaker2.7aa5624e.png"
+  }
+]);
